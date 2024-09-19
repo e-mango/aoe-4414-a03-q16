657e82322ee1cfec241ffe943228c6c74cf0e22a
e-mango <evans25@vt.edu>
2024-09-18 21:01:13 -0400

    Update sez_to_ecef.py

--- sez_to_ecef.py
+++ sez_to_ecef.py
@@ -54,26 +54,34 @@ else:
   exit()
 
 # write script below this line
+
+# Convert lat lon deg into rad
 o_lat_rad = o_lat_deg * math.pi/180.0
 o_lon_rad = o_lon_deg * math.pi/180.0
 
+# Calculate denom of fractions
 denom = calc_denom(E_E,o_lat_rad)
 
+# Calculate CE and SE
 c_E = R_E_KM/denom
 s_E = (R_E_KM*(1-E_E**2))/denom
 
+# Calculate rx ry rz components
 r_x_km = (c_E + o_hae_km)*math.cos(o_lat_rad)*math.cos(o_lon_rad)
 r_y_km = (c_E + o_hae_km)*math.cos(o_lat_rad)*math.sin(o_lon_rad)
 r_z_km = (s_E + o_hae_km)*math.sin(o_lat_rad)
 
+# Convert SEZ components to ECEF compatable
 conv_x_km = z_km*math.cos(o_lat_rad)*math.cos(o_lon_rad)+s_km*math.cos(o_lon_rad)*math.sin(o_lat_rad)-e_km*math.sin(o_lon_rad)
 conv_y_km = e_km*math.cos(o_lon_rad)+z_km*math.cos(o_lat_rad)*math.sin(o_lon_rad)+s_km*math.sin(o_lat_rad)*math.sin(o_lon_rad)
 conv_z_km = -s_km*math.cos(o_lat_rad)+z_km*math.sin(o_lat_rad)
 
+# Calculate ECEF components
 ecef_x_km = r_x_km + conv_x_km
 ecef_y_km = r_y_km + conv_y_km
 ecef_z_km = r_z_km + conv_z_km
 
+# Print ECEF components in km
 print(ecef_x_km)
 print(ecef_y_km)
 print(ecef_z_km)
